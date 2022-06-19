06a9962d2cfbc2bb902340887175873ffbf41e7f
EzinneJ <chimejoyez@gmail.com>
2022-06-19 07:38:09 +0100

    Implement interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/ezinne/springframework/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,16 @@
+package ezinne.springframework.sfgpetclinic.services;
+
+import ezinne.springframework.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
