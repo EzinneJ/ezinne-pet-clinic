49854ca7016ddb4a260d901a469f4227f7d8d366
EzinneJ <chimejoyez@gmail.com>
2022-06-28 09:37:08 +0100

    Created PetType Map Service Impl

---
+++ pet-clinic-data/src/main/java/ezinne/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package ezinne.springframework.sfgpetclinic.services;
+
+import ezinne.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
