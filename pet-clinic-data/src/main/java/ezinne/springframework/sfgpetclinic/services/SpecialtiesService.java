0985736bd2841a7817f54ca797d4b091cbed4629
EzinneJ <chimejoyez@gmail.com>
2022-06-28 16:08:32 +0100

    Created Specialty Map Service Impl

---
+++ pet-clinic-data/src/main/java/ezinne/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package ezinne.springframework.sfgpetclinic.services;
+
+import ezinne.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
