b89fdc2b876505cde472baf68d43fbd4b62d0eb4
EzinneJ <chimejoyez@gmail.com>
2022-06-29 07:13:02 +0100

    Created Specialties, added to Vets on startup (Bootstrap)

--- pet-clinic-data/src/main/java/ezinne/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/ezinne/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,7 +1,8 @@
 package ezinne.springframework.sfgpetclinic.services.map;
 
+import ezinne.springframework.sfgpetclinic.model.Specialty;
 import ezinne.springframework.sfgpetclinic.model.Vet;
-import ezinne.springframework.sfgpetclinic.services.CrudService;
+import ezinne.springframework.sfgpetclinic.services.SpecialtyService;
 import ezinne.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,12 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+    private final SpecialtyService specialtyService;
+
+    public VetServiceMap(SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -26,6 +33,15 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+
+        if (object.getSpecialties().size() > 0) {
+            object.getSpecialties().forEach(specialty -> {
+                if (specialty.getId() == null) {
+                    Specialty savedSpecialty = specialtyService.save(specialty);
+                    specialty.setId(savedSpecialty.getId());
+                }
+            });
+        }
         return super.save( object);
     }
 
--- pet-clinic-web/src/main/java/ezinne/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/ezinne/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,11 +1,9 @@
 package ezinne.springframework.sfgpetclinic.bootstrap;
 
-import ezinne.springframework.sfgpetclinic.model.Owner;
-import ezinne.springframework.sfgpetclinic.model.Pet;
-import ezinne.springframework.sfgpetclinic.model.PetType;
-import ezinne.springframework.sfgpetclinic.model.Vet;
+import ezinne.springframework.sfgpetclinic.model.*;
 import ezinne.springframework.sfgpetclinic.services.OwnerService;
 import ezinne.springframework.sfgpetclinic.services.PetTypeService;
+import ezinne.springframework.sfgpetclinic.services.SpecialtyService;
 import ezinne.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
@@ -17,15 +15,25 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
+    private final SpecialtyService specialtyService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
+        this.specialtyService = specialtyService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        int count = petTypeService.findAll().size();
+
+        if (count == 0) {
+            loadData();
+        }
+    }
+
+    private void loadData() {
         PetType dog = new PetType();
         dog.setName("Dog");
         PetType savedDogPetType = petTypeService.save(dog);
@@ -35,6 +43,18 @@ public class DataLoader implements CommandLineRunner {
         PetType savedCatPetType = petTypeService.save(cat);
 
         System.out.println("Loaded petTypes...");
+
+        Specialty radiology = new Specialty();
+        radiology.setDescription("Radiology");
+        Specialty savedRadiology = specialtyService.save(radiology);
+
+        Specialty dentistry = new Specialty();
+        dentistry.setDescription("Dentistry");
+        Specialty savedDentistry = specialtyService.save(dentistry);
+
+        Specialty surgery = new Specialty();
+        surgery.setDescription("Surgery");
+        Specialty savedSurgery = specialtyService.save(surgery);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Ezinne");
@@ -66,25 +86,24 @@ public class DataLoader implements CommandLineRunner {
         chidisPet.setName("bussu");
         owner2.getPets().add(chidisPet);
 
-       ownerService.save(owner2);
+        ownerService.save(owner2);
 
         System.out.println("Loaded owners...");
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Presh");
         vet1.setLastName("Babe");
+        vet1.getSpecialties().add(savedDentistry);
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Presh");
         vet2.setLastName("Girl");
+        vet2.getSpecialties().add(savedSurgery);
 
         vetService.save(vet2);
 
         System.out.println("Loaded vets...");
-
-
-
     }
 }
