faf99dfaa45d73ba1c2c11ae8a3439bce5225548
EzinneJ <chimejoyez@gmail.com>
2022-07-02 10:03:12 +0100

    Created a Visit Map Service

---
+++ pet-clinic-data/src/main/java/ezinne/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package ezinne.springframework.sfgpetclinic.services;
+
+import ezinne.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/ezinne/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,41 @@
+package ezinne.springframework.sfgpetclinic.services.map;
+
+import ezinne.springframework.sfgpetclinic.model.Vet;
+import ezinne.springframework.sfgpetclinic.model.Visit;
+import ezinne.springframework.sfgpetclinic.services.VetService;
+import ezinne.springframework.sfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if (visit.getPet() == null || visit.getPet().getOwner() == null ||
+                visit.getPet().getId() == null || visit.getPet().getPetType().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
