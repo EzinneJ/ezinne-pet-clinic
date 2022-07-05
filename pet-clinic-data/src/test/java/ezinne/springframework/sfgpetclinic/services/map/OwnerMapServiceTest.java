e4f42e43691aa5f65d459d0d5ae8e6244b95dfa2
EzinneJ <chimejoyez@gmail.com>
2022-07-05 08:11:02 +0100

    created Tests for OwnerMapService and implemented findByLastName in the OwnerMapService

---
+++ pet-clinic-data/src/test/java/ezinne/springframework/sfgpetclinic/services/map/OwnerMapServiceTest.java
@@ -0,0 +1,85 @@
+package ezinne.springframework.sfgpetclinic.services.map;
+
+import ezinne.springframework.sfgpetclinic.model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerMapServiceTest {
+
+    OwnerMapService ownerMapService;
+
+    Long ownerId = 1L;
+    final String lastName = "Chime";
+
+    @BeforeEach
+    void setUp() {
+        ownerMapService = new OwnerMapService(new PetMapService(), new PetTypeMapService());
+
+        ownerMapService.save(Owner.builder().id(ownerId).lastName(lastName).build());
+    }
+
+    @Test
+    void findAll() {
+        Set<Owner> ownerSet = ownerMapService.findAll();
+
+        assertEquals(1, ownerSet.size());
+    }
+
+    @Test
+    void deleteById() {
+        ownerMapService.deleteById(ownerId);
+
+        assertEquals(0, ownerMapService.findAll().size());
+
+    }
+
+    @Test
+    void delete() {
+        ownerMapService.delete(ownerMapService.findById(ownerId));
+
+        assertEquals(0, ownerMapService.findAll().size());
+    }
+
+    @Test
+    void save() {
+    Long id = 1L;
+        Owner owner2 = Owner.builder().id(id).build();
+        Owner savedOwner = ownerMapService.save(owner2);
+
+        assertEquals(id, savedOwner.getId());
+    }
+
+    @Test
+    void saveNoId() {
+        Owner savedOwner = ownerMapService.save(Owner.builder().build());
+
+        assertNotNull(savedOwner);
+        assertNotNull(savedOwner.getId());
+    }
+
+    @Test
+    void findById() {
+        Owner owner = ownerMapService.findById(ownerId);
+
+        assertEquals(ownerId, owner.getId());
+    }
+
+    @Test
+    void findByLastName() {
+        Owner chime = ownerMapService.findByLastName(lastName);
+
+        assertNotNull(chime);
+        assertEquals(ownerId, chime.getId());
+    }
+    @Test
+    void findByLastNameNotFound() {
+        Owner chime = ownerMapService.findByLastName("foo");
+
+        assertNull(chime);
+
+    }
+}
--- pet-clinic-web/src/test/java/ezinne/springframework/ezinnepetclinic/EzinnePetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/ezinne/springframework/ezinnepetclinic/EzinnePetClinicApplicationTests.java
@@ -2,8 +2,11 @@ package ezinne.springframework.ezinnepetclinic;
 
 import ezinne.springframework.sfgpetclinic.EzinnePetClinicApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = {EzinnePetClinicApplication.class})
 class EzinnePetClinicApplicationTests {
 
